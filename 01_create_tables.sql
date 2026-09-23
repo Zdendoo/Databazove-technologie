3979bcf69500296975ce1ec33c13a503f28d10d3
Zdendoo <denis.valkovic@student.ukf.sk>
2026-09-23 14:22:13 +0200

    SQL na tvorbu databazy a tables

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -11,7 +11,6 @@ CREATE TABLE products (
     product_id VARCHAR(20) PRIMARY KEY,
     category VARCHAR(50),
     sub_category VARCHAR(50),
-    region VARCHAR(50),
     product_name VARCHAR(100)
 );
 
